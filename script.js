e4ab0a4a1a6a30eab6a5383c538a2e95010bfb33
Rodrigo <ronico041012@gmail.com>
2020-09-01 00:24:13 -0300

    the size of the grid can be change

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const containter=document.querySelector('.container');
 let x=Math.floor((Math.random()*0xfff)).toString(16).padStart(4,'#');
-/*let k=prompt("How many grids per column and row ?")
-while(typeof k!='number'){
+let k=prompt("How many grids per column and row ?");//determine the size of the grid
+/*while(typeof k!='number'){
 let k=prompt("How many grids per column and row ?")
 }*/
-for(let i=0;i<4;i++){
-    for(let j=0;j<4;j++){
+containter.style.gridTemplateRows=`repeat(${k},${100/k}%)`;//cange the amounto of rows
+containter.style.gridTemplateColumns=`repeat(${k},${100/k}%)`;//change the amount of columns
+for(let i=0;i<k;i++){
+    for(let j=0;j<k;j++){
     let div=  document.createElement('div');
     div.classList.add('box');
     div.style.backgroundColor=x;
@@ -13,7 +15,7 @@ for(let i=0;i<4;i++){
     }
 }   
 
-function changeColor(){
+function changeColor(){//change the color of each div
     let x=Math.floor((Math.random()*0xfff)).toString(16).padStart(4,'#');
     this.style.backgroundColor=x;
     console.log(`este numero :`+x);

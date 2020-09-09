1a4bb1c423a23a6c5f39e0584f99b061e5606b10
Rodrigo <ronico041012@gmail.com>
2020-09-08 23:47:01 -0300

    add the create grid function and centered everything

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const containter=document.querySelector('.container');
 let x=Math.floor((Math.random()*0xfff)).toString(16).padStart(4,'#');
-let k=prompt("How many grids per column and row ?");//determine the size of the grid
+
 /*while(typeof k!='number'){
 let k=prompt("How many grids per column and row ?")
 }*/
-containter.style.gridTemplateRows=`repeat(${k},${100/k}%)`;//cange the amounto of rows
-containter.style.gridTemplateColumns=`repeat(${k},${100/k}%)`;//change the amount of columns
+/*function createGrid(){
+    let k=prompt("How many grids per column and row ?");
+    containter.style.gridTemplateRows=`repeat(${k},1fr)`;//cange the amounto of rows
+containter.style.gridTemplateColumns=`repeat(${k},1fr)`;//change the amount of columns
 for(let i=0;i<k;i++){
     for(let j=0;j<k;j++){
     let div=  document.createElement('div');
@@ -13,15 +15,54 @@ for(let i=0;i<k;i++){
     div.style.backgroundColor=x;
     containter.appendChild(div);    
     }
-}   
+} 
+}*/
+function removeAllChildNodes(parent){//removes all the divs so it can create a new div of a diferent size
+     
+    while(parent.firstChild){
+          
+         parent.removeChild(parent.firstChild);
+    } 
+ }
 
-function changeColor(){//change the color of each div
-    let x=Math.floor((Math.random()*0xfff)).toString(16).padStart(4,'#');
-    this.style.backgroundColor=x;
-    console.log(`este numero :`+x);
-}
-
-const boxes=document.querySelectorAll('.box');
+function createGrid(k){//creates a grid of kxk size
+    if(typeof k=='number'){
+        
+        removeAllChildNodes(containter);
+        containter.style.gridTemplateRows=`repeat(${k},1fr)`;//cange the amounto of rows
+        containter.style.gridTemplateColumns=`repeat(${k},1fr)`;//change the amount of columns
+        for(let i=0;i<k;i++){
+            for(let j=0;j<k;j++){
+                let div=  document.createElement('div');
+                div.classList.add('box');
+                div.style.backgroundColor=x;
+                containter.appendChild(div);    
+            }
+        }
+         
+    }
+    const boxes=document.querySelectorAll('.box');//make all squares color changeable
     boxes.forEach((box) => {
         box.addEventListener('mouseover',changeColor);
     });
+}        
+function changeColor(){//change the color of each grid element
+    let x=Math.floor((Math.random()*0xfff)).toString(16).padStart(4,'#');
+    this.style.backgroundColor=x;
+     
+}
+
+function newGrid(){//creates a new grid 
+    let rowsAndColumns=parseInt(prompt("Chose the size of the gride(row and columns)"));//determine the size of the grid
+     
+    createGrid(rowsAndColumns);
+    }
+/*function restartPage(){
+    window.location.reload();
+}*/
+createGrid(8);
+
+
+const restartButton=document.querySelector('#restart-button');
+restartButton.addEventListener('click',newGrid);
+/*window.addEventListener('onbeforeprint',createGrid);*/
